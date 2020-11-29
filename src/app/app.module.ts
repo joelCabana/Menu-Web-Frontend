1aab1722da27798ffab80fa415c50e9421d44b37
joelCabana <joelxcabana@gmail.com>
2020-11-29 12:51:55 -0300

    Feat: Create new proyect, add components (navigation and menu), add boostwatch,bootstrap,jquery and popper.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
